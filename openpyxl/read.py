ffbb456b606135a3bd682309c37dd9a34229a3c1
KadiSam02 <kadimas@xavier.edu>
2024-09-25 21:36:37 -0400

    good

--- openpyxl/read.py
+++ openpyxl/read.py
@@ -1,2 +1,9 @@
 from openpyxl import load_workbook
 
+excel_file = 'FoundHouse.xlsx'    #excel file path
+wb = load_workbook(excel_file)
+
+ws = wb.active
+
+for row in ws.iter_rows(values_only=True):
+    print(row)  # Print the row values
