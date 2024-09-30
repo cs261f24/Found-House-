98de9adaca516d1c7bff09e60d3f32c407790181
KadiSam02 <kadimas@xavier.edu>
2024-09-29 22:27:49 -0400

    creating the search function

--- openpyxl/read.py
+++ openpyxl/read.py
@@ -4,8 +4,34 @@ from openpyxl import Workbook, load_workbook
 
 book = load_workbook("FoundHouse.xlsx")
 sheet = book.active
+sheet2 = book.active
+sheet_names = book.sheetnames 
+print(sheet2) #print sheet 2
+print(sheet) #print sheet 1
+print(sheet_names) #print all sheetname
 
-sheet_names = book.sheetnames
-print(sheet)
-print(sheet_names)
-print(sheet["A2"].value)
+# Save the Excel file
+
+
+#search function(testing column B and C)
+
+def search():
+    sheet_select = input("Enter the sheet you want to search: ")
+    column = input("Enter the column you want to search: ").upper()
+    search = input("Enter what you want to search: ")
+    
+
+book.save("FoundHouse.xlsx")
+
+
+def main():
+    
+    search()
+
+
+    
+    
+    
+
+
+
