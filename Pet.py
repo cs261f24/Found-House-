6bffff9866e8346537a629876aa5a8a0fd246ac2
Qew Shugars <shugarsq@xavier.edu>
2024-10-24 14:58:21 -0400

    The python Pet.py file that reads the spreadsheet. Has an error where it deletes the other sheets inside of the spreadsheet so it should not be ran yet. Pushing so we all have something to look at.

--- Pet.py
+++ Pet.py
@@ -1,6 +1,37 @@
-class Pet:
-    def __init__(self):
-        self.attributes = []
+import pandas as pd
+import numpy as np
+# import openpyxl as op
+# from openpyxl import workbook
+from openpyxl import load_workbook
 
-    def add_attribute(self, datum):
-        self.attributes.append(datum)
+# SETS DF TO READ THE SPREADSHEET
+df = pd.read_excel(''' input excel spreadsheet file here ''')
+
+# REMOVES NAN FROM BLANK COLUMNS
+df = df.fillna('')
+df.dropna(inplace=True)
+df.to_excel(''' input excel spreadsheet file here ''', index=False)
+df1 = df.replace(np.nan, '', regex=True)
+
+# LOADS THE WORKBOOK IF SHEET IS USED
+excelBook = load_workbook(''' input spreadsheet file here ''')
+
+writer = pd.ExcelWriter(''' input spreadsheet file here ''', engine='openpyxl')
+
+with pd.ExcelWriter(''' input spreadsheet file here ''') as writer:
+    # Saving my the excel spreadsheet as a base.
+    writer.book = excelBook
+    writer.sheets = dict((ws.title, ws) for ws in excelBook.worksheets)
+
+    # Saves the workbook
+    writer.save()
+
+
+# MAKES A DATAFRAME
+print("------------")
+
+# PRINTS WHOLE LIST USING TO_STRING
+print(df.to_string())
+print("------------")
+
+print(df)
