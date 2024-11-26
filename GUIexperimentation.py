490a09e9a1fff018a3543cc26a1dc3c0cb575487
KadiSam02 <kadimas@xavier.edu>
2024-11-26 14:37:49 -0500

    UPDATED GUI, BUTTON NOW SMALLER?

--- GUIexperimentation.py
+++ GUIexperimentation.py
@@ -1,9 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QLineEdit, QPushButton, QComboBox, QTableWidget, QTableWidgetItem, QTextEdit
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout, QLineEdit, QPushButton, QComboBox, QTableWidget, QTableWidgetItem, QTextEdit, QInputDialog
 from openpyxl import load_workbook
 import pandas as pd
 from read import search_single_value, search_in_workbook
-
+#from rows import add_column, remove_column, add_row, remove_row
 class Main(QWidget):
     def __init__(self):
         super(Main, self).__init__()
@@ -12,13 +12,13 @@ class Main(QWidget):
         layout = QVBoxLayout()
         self.setWindowTitle("Found House - For the Pets")
         self.setGeometry(100, 100, 900, 600)  # Set the window size
-        self.setLayout(layout)
+        self.setLayout(layout) 
 
         # Selecting the respective sheets
         self.sheet_input = QComboBox()
         sheets = load_workbook("FoundHouse.xlsx").sheetnames
         self.sheet_input.addItems(sheets)
-        layout.addWidget(self.sheet_input)
+        layout.addWidget(self.sheet_input) 
 
         # Selecting animals
         self.animal_input_label = QLabel("Select Species:")
@@ -44,26 +44,35 @@ class Main(QWidget):
         self.filter_button.clicked.connect(self.search_in_workbook_button_clicked)
         layout.addWidget(self.filter_button)
 
-        self.results_table = QTableWidget()
-        layout.addWidget(self.results_table)
+         
+        self.result_label = QTextEdit()
+        layout.addWidget(self.result_label) 
 
         # Add rows and columns
         self.add_rows_button = QPushButton("Add Rows")
+        #adjust size of button
+        sheet_name = self.sheet_input.currentText()
+        self.add_rows_button.setFixedWidth(150)
+        #self.add_rows_button.clicked.connect(self.add_rows_button_clicked)
         layout.addWidget(self.add_rows_button)
 
         self.add_columns_button = QPushButton("Add Columns")
+        self.add_columns_button.setFixedWidth(150)
         layout.addWidget(self.add_columns_button)
 
         # Remove rows and columns
         self.remove_rows_button = QPushButton("Remove Rows")
+        self.remove_rows_button.setFixedWidth(150)
         layout.addWidget(self.remove_rows_button)
 
         self.remove_columns_button = QPushButton("Remove Columns")
+        self.remove_columns_button.setFixedWidth(150)
         layout.addWidget(self.remove_columns_button)
 
         # Result label
-        self.result_label = QTextEdit()
-        layout.addWidget(self.result_label)
+        self.results_table = QTableWidget()
+        layout.addWidget(self.results_table) 
+      
 
         try:
             self.workbook = load_workbook("FoundHouse.xlsx")
@@ -90,6 +99,12 @@ class Main(QWidget):
             self.result_label.setText(result)
         else:
             self.result_label.setText("not found")
+            
+    def add_rows_button_clicked(self):
+        sheet_name = self.sheet_input.currentText()
+        user_input = QInputDialog.getInt(self, "Add Rows", "Enter the number of rows to add:")
+        
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Main()
